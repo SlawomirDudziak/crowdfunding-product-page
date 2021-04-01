f4254a725de208a06ff3221b999e74ddca106881
SlawomirDudziak <50486478+SlawomirDudziak@users.noreply.github.com>
2021-04-01 20:16:09 +0200

    Changed onclick to addEventListener

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
-function openForm() {
-    document.getElementById("modal").style.display = "block";
-}
+document.addEventListener('DOMContentLoaded', () => {
 
-function closeForm() {
-    document.getElementById("modal").style.display = "none";
-}
+    const buttons = document.querySelectorAll('.green-button');
+    const closeButton = document.querySelector('.close-modal');
+
+    closeButton.addEventListener('click', () => {
+        closeForm();
+    });
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            openForm();
+        });
+    });
+
+    function openForm() {
+        document.getElementById("modal").style.display = "block";
+    }
+    
+    function closeForm() {
+        document.getElementById("modal").style.display = "none";
+    }
+})
