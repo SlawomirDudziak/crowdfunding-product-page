3d3b07d0d9fdb7d71b8e851549c4670d3e80930d
SlawomirDudziak <50486478+SlawomirDudziak@users.noreply.github.com>
2021-04-02 20:20:39 +0200

    Added option selection on h3 click

--- main.js
+++ main.js
@@ -2,6 +2,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const buttons = document.querySelectorAll('.green-button');
     const closeButton = document.querySelector('.close-modal');
+    const option1Header = document.getElementById('option1');
+    const option2Header = document.getElementById('option2');
+    const option3Header = document.getElementById('option3');
+
+    option1.addEventListener('click', () => {
+        removeSelection();
+        const aboutOption = option1Header.parentElement.parentElement;
+        aboutOption.classList.add("selected");
+    })
+
+    option2.addEventListener('click', () => {
+        removeSelection();
+        const aboutOption = option2Header.parentElement.parentElement;
+        aboutOption.classList.add("selected");
+    })
+
+    option3.addEventListener('click', () => {
+        removeSelection();
+        const aboutOption = option3Header.parentElement.parentElement;
+        aboutOption.classList.add("selected");
+    })
 
     closeButton.addEventListener('click', () => {
         closeForm();
@@ -19,5 +40,10 @@ document.addEventListener('DOMContentLoaded', () => {
     
     function closeForm() {
         document.getElementById("modal").style.display = "none";
+    }
+
+    function removeSelection() {
+        const options = document.querySelectorAll('.about-option');
+        options.forEach(option => option.classList.remove("selected"));
     }
 })
